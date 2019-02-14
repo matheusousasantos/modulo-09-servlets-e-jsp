b95679c1e7dce7de533be09cd18696f43feef884
Matheus Sousa <matheus.ads.study@gmail.com>
2019-02-14 08:23:16 -0300

    Atualização do curso #03 - Upload de imagens - Capturando imagem no servidor (correção) - Parte 3

--- src/servlet/Usuario.java
+++ src/servlet/Usuario.java
@@ -1,15 +1,19 @@
 package servlet;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.SQLException;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
@@ -21,6 +25,7 @@ import beans.BeanCursoJsp;
 import dao.UsuarioDAO;
 
 @WebServlet("/salvarUsuario")
+@MultipartConfig
 public class Usuario extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -117,20 +122,17 @@ public class Usuario extends HttpServlet {
 				
 				if(ServletFileUpload.isMultipartContent(request)) {
 					
-					List<FileItem> fileIntens = 
-							new ServletFileUpload(new DiskFileItemFactory()).parseRequest(request);
+					Part imagemFoto = request.getPart("foto");
 					
-					for (FileItem fileItem : fileIntens) {
-						if(fileItem.getFieldName().equals("foto")) {
-							
-							String fotoBase64 = new Base64().encodeBase64String(fileItem.get());
-							String contentType = fileItem.getContentType();
-							usuario.setFotoBase64(fotoBase64);
-							usuario.setContentType(contentType);
-						}
-					}
+					String fotoBase64 = new Base64().
+							encodeBase64String(converteStreamParaByte(imagemFoto.getInputStream()));
+					
+					usuario.setFotoBase64(fotoBase64);
+					usuario.setContentType(imagemFoto.getContentType());
 					
 				}
+				
+				
 
 				String msg = null;
 				boolean podeInserir = true;
@@ -197,5 +199,21 @@ public class Usuario extends HttpServlet {
 
 		}
 	}
-
+	
+	/*Converte a entrada de fluxo de dados da imagem para um array de byte(byte[]) */
+	private byte[] converteStreamParaByte(InputStream imagem) throws Exception{
+	
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		
+		int reads = imagem.read();
+		
+		while(reads != -1) {
+			baos.write(reads);
+			reads = imagem.read();
+		}
+		
+		return baos.toByteArray();
+		
+	}
+	
 }
